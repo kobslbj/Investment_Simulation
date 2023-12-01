29b71ff1c85b3731224b42298fa2a1693e0c3687
justinli <kobs666666@gmail.com>
2023-12-01 16:54:44 +0800

    feat/cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const userRoutes = require("./routes/userRoute");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", userRoutes);
 app.listen(port, () => {
